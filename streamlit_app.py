b5c248707d76a86562d18eeaf940b9c9d7cc069d
gurupatra <35889232+gurupatra@users.noreply.github.com>
2024-10-17 20:45:48 +0100

    updated the code to display the JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients:
     st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 fruityvice_response = requests.get("https://www.googleapis.com/customsearch/v1?key=AIzaSyDGlAry0tpNXlgpMJyouELBKkxI71YFETI&cx=017576662512468239146:omuauf_lfve&q=lectures")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
